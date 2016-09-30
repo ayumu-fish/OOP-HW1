0b1945fe55a2daf2ea263c9dc0c7e4b1dbae72b2
user <user@FC07>
2016-09-30 09:18:15 +0800

    it->"her" Cannot->can't

--- src/test/Test.java
+++ src/test/Test.java
@@ -2,7 +2,10 @@ package test;
 
 public class Test {
 	public static void main(String[] args){
-		System.out.println("Hello Word");
+		String senten="Let it go!Let it go! Cannot hold it back anymore";
+		senten=senten.replace("it", "\"her\"");
+		senten=senten.replace("Cannot", "Can\'t");
+		System.out.println(senten);
 	}
 
 }
